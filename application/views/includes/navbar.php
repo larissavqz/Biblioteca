d2aa07ecd6373dc597b141612659c569bd44a350
Gustavo Agibert <gstv270@hotmail.com>
2019-06-17 21:25:15 -0300

    ajustes finais V-1.0

--- application/views/includes/navbar.php
+++ application/views/includes/navbar.php
@@ -4,31 +4,32 @@
 		<span class="navbar-toggler-icon"></span>
 	</button>
 	<div class="collapse navbar-collapse" id="navbarSupportedContent">
-	<ul class="navbar-nav mr-auto">
-		<li class="nav-item active">
-			<a class="nav-link" href="<?=base_url().'pagina-inicial'?>">Home <span class="sr-only">(current)</span></a>
-		</li>
-		<?php
-		if(isset($this->session->userdata['logged_in']['ctps']))
-		{
-			echo   '<li class="nav-item">
-				  	<a class="nav-link" href="'.base_url().'cadastrar-usuario">Cadastrar Usuário</a>
-					</li>
-					<li class="nav-item">
-				  	<a class="nav-link" href="'.base_url().'cadastrar-administrador">Cadastrar Administrador</a>
-					</li>';
-		}
-		var_dump($this->session->userdata['logged_in']['cpf']);		
-		?>
-		<li class="nav-item">
-			<a class="nav-link" href="<?=base_url().'cadastrar-livro'?>">Cadastrar Livro</a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link" href="<?=base_url().'emprestimos'?>">Emprestimos</a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link" href="<?=base_url().'logout'?>">Logout</a>
-		</li>		  
-	</ul>
+		<ul class="navbar-nav mr-auto">
+			<li class="nav-item active">
+				<a class="nav-link" href="<?=base_url().'pagina-inicial'?>">Home <span class="sr-only">(current)</span></a>
+			</li>
+			<?php
+			if(isset($this->session->userdata['logged_in']['ctps']))
+			{
+				echo   '<li class="nav-item">
+						<a class="nav-link" href="'.base_url().'cadastrar-usuario">Cadastrar Usuário</a>
+						</li>
+						<li class="nav-item">
+						<a class="nav-link" href="'.base_url().'cadastrar-administrador">Cadastrar Administrador</a>
+						</li>
+						<li class="nav-item">
+						<a class="nav-link" href="'.base_url().'cadastrar-livro">Cadastrar Livro</a>
+						</li>';
+			}?>			
+			<li class="nav-item">
+				<a class="nav-link" href="<?=base_url().'emprestimos'?>">Emprestimos</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="<?=base_url().'logout'?>">Logout</a>
+			</li>				  
+		</ul>
+		<p class="nav-item">
+			<a class="nav-link" style="text-decoration:none !important; text-align: right;">Bem-vindo <?=$this->session->userdata['logged_in']['nome'];?></a>
+		</p>
 	</div>
 </nav>
--- application/views/includes/navbar_no_itens.php
+++ application/views/includes/navbar_no_itens.php
@@ -3,7 +3,5 @@
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 	</button>
-	<div class="collapse navbar-collapse" id="navbarSupportedContent">
-	<?php  var_dump($this->session->userdata); ?>
-	</div>
+	<div class="collapse navbar-collapse" id="navbarSupportedContent"></div>
 </nav>
